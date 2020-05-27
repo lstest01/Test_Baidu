db66543c3c0f77eedb7cabf5c996c4f6a16f56ad
liushuai <lius923@126.com>
2020-05-28 00:49:57 +0800

    更新 - 刘帅

--- ToTest/Baidu_Test_01.py
+++ ToTest/Baidu_Test_01.py
@@ -1,4 +1,6 @@
 # from Data.SeachData import testData
+import allure
+
 from My_Page import Page
 import pytest
 from My_Page.baidu_Seach import Seach
@@ -13,9 +15,11 @@ class Test_01:
     # def test0001(self):
     #     print("*"*50)
     #     print(testData())
-
+    @allure.step(title="标题:测试百度搜索")
+    @pytest.allure.severity(pytest.allure.severity_level.BLACKER)
     @pytest.mark.parametrize("td",[1,2,3,4,5])
     def test_01(self,td):
+        allure.attach("测试输入和搜索", "搜索列表[1,2,3,4,5]")
         Seach().seach(td)
         # assert td in result
 
--- ToTest/Baidu_Test_02.py
+++ ToTest/Baidu_Test_02.py
@@ -1,3 +1,4 @@
+import allure
 import pytest
 
 class Test_02:
@@ -8,17 +9,25 @@ class Test_02:
     def teardown_class(self):
         print("---------teardown_class执行--------------")
 
-    # def setup(self):
-    #     print("---------setup执行--------------------")
-    # def teardown(self):
-    #     print("---------teardown执行-----------------")
-    # @pytest.fixture()
-    # def test01(self):
-    #     print("----------------test01---------------")
+    def setup(self):
+        print("---------setup执行--------------------")
+    def teardown(self):
+        print("---------teardown执行-----------------")
+    @pytest.fixture()
+    def test01(self):
+        print("----------------test01---------------")
     @pytest.mark.parametrize('t',[1,2,3,4,5])
-    # @pytest.mark.usefixtures("test01")
+    @pytest.mark.usefixtures("test01")
     def test02(self,t):
         print(f"----------------{t}---------------")
-    #
-    # def test03(self):
-    #     print("----------------test03---------------")
+
+    @allure.step(title="标题：无关紧要的测试")
+    @pytest.allure.severity(pytest.allure.severity_level.TRIVIAL)
+    def test03(self):
+        allure.attach("描述主题：打印", "描述内容：随便打印一些内容吧")
+        print("----------------test03---------------")
+
+    @allure.step(title="一个assert失败的测试")
+    def test04(self):
+        print("")
+        assert False
